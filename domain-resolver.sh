c256bf9df7afe091a238eb98becbb3644f87b370
Controller Vrelk <vrelk@users.noreply.github.com>
2020-04-20 05:18:28 -0400

    Update domain-resolver.sh

--- domain-resolver.sh
+++ domain-resolver.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
+allowed_hosts="/etc/nginx/allowedhosts.conf"
 filename="$1"
+updated=false
+temp_file=$(mktemp)
 while read -r line
 do
         ddns_record="$line"
         if [[ !  -z  $ddns_record ]]; then
                 resolved_ip=`getent ahosts $line | awk '{ print $1 ; exit }'`
+                if ! grep -q $resolved_ip "$allowed_hosts"; then
+                    updated=true
+                fi
                 if [[ !  -z  $resolved_ip ]]; then
-                        echo "allow $resolved_ip;# from $ddns_record"
+                        echo "allow $resolved_ip; #from $ddns_record" >> $temp_file
                 fi
         fi
 done < "$filename"
+if $updated; then
+        cp -f $temp_file $allowed_hosts
+        service nginx reload
+fi
+rm ${temp_file}
